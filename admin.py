3719b8afe08d5b198fba0d451aa2c0f4099fb79c
valensab <81138139+valensab@users.noreply.github.com>
2022-05-08 22:47:00 -0500

    Delete admin.py

--- admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from publications.models import Publication
-
-# Register your models here.
-class PublicationAdmin(admin.ModelAdmin):
-    list_display = ['id_publication', 'user', 'state']
-
-admin.site.register(Publication, PublicationAdmin)
