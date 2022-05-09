281d85f3b2a8db4e1f7f718a7a437c595fe41e8d
valensab <81138139+valensab@users.noreply.github.com>
2022-05-08 22:47:16 -0500

    Add files via upload

--- publications/admin.py
+++ publications/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-
 from publications.models import Publication
 
 # Register your models here.
+class PublicationAdmin(admin.ModelAdmin):
+    list_display = ['id_publication', 'user', 'state']
 
-admin.site.register(Publication)
+admin.site.register(Publication, PublicationAdmin)
